5994fa0fc8557b46c1e7e6d48a0b1c47c2ccbea9
omgutty <om.gutty@gmail.com>
2026-08-27 09:14:44 +0530

    feat: oops practice

---
+++ chapter_11_Python_Learning/Task/classpractice.py
@@ -0,0 +1,23 @@
+class wife:
+    name=None
+    age=None
+    gender=None
+
+
+
+    def talk(self):
+        print("I can alk")
+
+    def fight(self):
+        print("I can fight"
+
+          )
+
+def goout():
+    print("can goout with intimation")
+
+gow= wife()
+
+
+print(gow.age)
+print(gow.talk())
