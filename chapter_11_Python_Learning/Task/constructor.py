8c778755b0fe7b44245c14ace8fa604f7794e6d4
omgutty <om.gutty@gmail.com>
2026-08-27 11:14:53 +0530

    oops complete prorams

---
+++ chapter_11_Python_Learning/Task/constructor.py
@@ -0,0 +1,13 @@
+class mobile:
+    model=None
+
+    def __init__(self,model):
+        print("mobile constuctor ")
+        self.model=model
+        print(self.model)
+        
+
+
+iphone=mobile("10")
+#iphone.model="10"
+
